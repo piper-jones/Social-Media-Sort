f600048fb7bde627848b5bdb746632b2be75c611
piper-jones <139887562+piper-jones@users.noreply.github.com>
2024-08-04 16:33:22 -0400

    Constructors for post objects

--- Post.cpp
+++ Post.cpp
@@ -1,5 +1,33 @@
-//
-// Created by piper on 8/4/2024.
-//
-
 #include "Post.h"
+#include <iostream>
+using namespace std;
+
+//default constructor
+Post::Post(){}
+//parameterized constructor
+Post::Post(int postID, string postType, string postContent, string timeStamp, string date, string weekdayType,
+           string time, string timePeriod, int likes, int comments, int shares, int impressions, int reach,
+           int engagementRate, int audienceAge, string ageGroup, string audienceGender, string location,
+           string continent, string audienceInterests, string sentiment){
+    this->postID = postID;
+    this->postType = postType;
+    this->postContent = postContent;
+    this->timeStamp = timeStamp;
+    this->date = date;
+    this->weekdayType = weekdayType;
+    this->time = time;
+    this->timePeriod = timePeriod;
+    this->likes = likes;
+    this->comments = comments;
+    this->shares = shares;
+    this-> impressions = impressions;
+    this->reach = reach;
+    this->engagementRate = engagementRate;
+    this->audienceAge = audienceAge;
+    this->ageGroup = ageGroup;
+    this->audienceGender = audienceGender;
+    this->location = location;
+    this->continent = continent;
+    this->audienceInterests = audienceInterests;
+    this->sentiment = sentiment;
+}
--- Post.h
+++ Post.h
@@ -1,14 +1,41 @@
-//
-// Created by piper on 8/4/2024.
-//
-
-#ifndef SOCIAL_MEDIA_SORT_POST_H
-#define SOCIAL_MEDIA_SORT_POST_H
+#pragma once
+#include <iostream>
+using namespace std;
 
 
 class Post {
+public:
+    int postID;
+    string postType;
+    string postContent;
+    string timeStamp;
+    string date;
+    string weekdayType;
+    string time;
+    string timePeriod;
+    int likes;
+    int comments;
+    int shares;
+    int impressions;
+    int reach;
+    int engagementRate;
+    int audienceAge;
+    string ageGroup;
+    string audienceGender;
+    string location;
+    string continent;
+    string audienceInterests;
+    //skipped campaign ID here
+    string sentiment;
+    //skipped influence ID here
 
+    //default constructor
+    Post();
+    //parameterized constructor
+    Post(int postID, string postType, string postContent, string timeStamp, string date, string weekdayType,
+         string time, string timePeriod, int likes, int comments, int shares, int impressions, int reach,
+         int engagementRate, int audienceAge, string ageGroup, string audienceGender, string location,
+         string continent, string audienceInterests, string sentiment);
 };
 
 
-#endif //SOCIAL_MEDIA_SORT_POST_H
