0e54948f0d2b4997320b19e0b8e3d52b4e9563bd
dang1 <dustinn1204@gmail.com>
2024-08-04 23:49:40 -0400

    Fixed Read Code with updates Post Properties

--- read.cpp
+++ read.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-vector<Post > readFile(vector<Post> &data) {
+vector<Post> readFile(vector<Post> &data) {
     string file_path = "C:\\Users\\dang1\\CLionProjects\\Social-Media-Sort\\social_media_engagement_data.csv";
 
     // Open the file
@@ -26,7 +26,7 @@ vector<Post > readFile(vector<Post> &data) {
         cerr << "Failed to read the header line." << endl;
         return data;
     }
-    cout << "Headers: " << line << endl;
+
 
     // Read each line of the file
     while (getline(file, line)) {
@@ -34,11 +34,11 @@ vector<Post > readFile(vector<Post> &data) {
         string cell;
 
         // Variables to store data for each column
-        string platform = "";
-        string post_id = "";
-        string post_type = "";
-        string post_content = "";
-        string post_timestamp = "";
+        string platform;
+        string post_id;
+        string post_type;
+        string post_content;
+        string post_timestamp;
         int likes = 0;
         int comments = 0;
         int shares = 0;
@@ -60,20 +60,13 @@ vector<Post > readFile(vector<Post> &data) {
             getline(ss, post_type, ',');
             getline(ss, post_content, ',');
             getline(ss, post_timestamp, ',');
-            getline(ss, cell, ',');
-            likes = stoi(cell);
-            getline(ss, cell, ',');
-            comments = stoi(cell);
-            getline(ss, cell, ',');
-            shares = stoi(cell);
-            getline(ss, cell, ',');
-            impressions = stoi(cell);
-            getline(ss, cell, ',');
-            reach = stoi(cell);
-            getline(ss, cell, ',');
-            engagement_rate = stof(cell);
-            getline(ss, cell, ',');
-            audience_age = stoi(cell);
+            getline(ss, cell, ','); likes = stoi(cell);
+            getline(ss, cell, ','); comments = stoi(cell);
+            getline(ss, cell, ','); shares = stoi(cell);
+            getline(ss, cell, ','); impressions = stoi(cell);
+            getline(ss, cell, ','); reach = stoi(cell);
+            getline(ss, cell, ','); engagement_rate = stof(cell);
+            getline(ss, cell, ','); audience_age = stoi(cell);
             getline(ss, audience_gender, ',');
             getline(ss, audience_location, ',');
             getline(ss, audience_interests, ',');
@@ -84,16 +77,13 @@ vector<Post > readFile(vector<Post> &data) {
             string temp = post_timestamp;
             temp.erase(remove(temp.begin(), temp.end(), ':'), temp.end());
 
-
-            data.push_back(Post(platform, post_id, post_type, post_content, post_timestamp, stof(temp), likes, comments,
-                                shares, impressions, reach, engagement_rate, audience_age, audience_gender,
-                                audience_location, audience_interests, sentiment));
-
-
-        } catch (const std::exception &e) {
+            data.push_back(Post(platform, post_id, post_type, post_content, post_timestamp, stof(temp), likes, comments, shares, impressions, reach, engagement_rate, audience_age, audience_gender, audience_location, audience_interests, sentiment));
+        } catch (const std::exception& e) {
             cerr << "Error parsing line: " << line << "\nException: " << e.what() << endl;
             // Optionally continue to the next line
             continue;
         }
     }
+
+    return data;
 }
--- read.h
+++ read.h
@@ -6,6 +6,6 @@
 
 #ifndef SOCIAL_MEDIA_SORT_READ_H
 #define SOCIAL_MEDIA_SORT_READ_H
-vector<Post> readFile(std::vector<Post *>& posts);
+vector<Post> readFile(std::vector<Post>& posts);
 
 #endif //SOCIAL_MEDIA_SORT_READ_H
