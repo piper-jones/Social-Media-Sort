53cc2bbd182914d13f3965741723d8477733ea99
dang1 <dustinn1204@gmail.com>
2024-08-06 13:56:05 -0400

    Fixed read.cpp and main to read in data properly

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <vector>
 #include "gui.h"
+#include "read.h"
+#include "Post.h"
+
 
 
 int main() {
+    std::vector<Post> data;
+    readFile(data);
+
     gui mediaMirror;
     mediaMirror.run();
 
